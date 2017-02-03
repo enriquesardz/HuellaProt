ba4aeaf2a12cb12d3c0ae52f7235168e69878abc
Enrique Salas Rodriguez <enriquesaalas@gmail.com>
2017-02-03 15:04:18 -0600

    First commit Add main screen, still missing visual details Add Report screen, missing visual details and button configuration Add Scanning screen

---
+++ app/src/main/java/com/example/ensardz/pantallasenrique/Escaneo.java
@@ -0,0 +1,13 @@
+package com.example.ensardz.pantallasenrique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Escaneo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escaneo);
+    }
+}
