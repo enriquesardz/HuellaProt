e30be6a410ad01a141306c8f7565f07e134171fb
Enrique Salas Rodriguez <enriquesaalas@gmail.com>
2017-02-03 15:34:33 -0600

    Modify Main screen for navigation purposes Modify Scanner screen, moved its content to the Report screen, fixed the buttons and added functionality for testing

--- app/src/main/java/com/example/ensardz/pantallasenrique/MainActivity.java
+++ app/src/main/java/com/example/ensardz/pantallasenrique/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ensardz.pantallasenrique;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -97,5 +98,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void iniciarRecorrido(View view){
+        Intent intent = new Intent(this, NuevoReporte.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/ensardz/pantallasenrique/NuevoReporte.java
+++ app/src/main/java/com/example/ensardz/pantallasenrique/NuevoReporte.java
@@ -2,6 +2,8 @@ package com.example.ensardz.pantallasenrique;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class NuevoReporte extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class NuevoReporte extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_reporte);
+    }
+
+    public void aceptarOnClick(View view){
+        Toast.makeText(this, "Has presionado aceptar", Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelarOnClick(View view){
+        Toast.makeText(this, "Has presionado cancelar", Toast.LENGTH_SHORT).show();
     }
 }
