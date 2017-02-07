2659849b373d514d757eebf92ce97cfdf17c115d
Enrique Salas Rodriguez <enriquesaalas@gmail.com>
2017-02-07 14:04:32 -0600

    Fix The app bar no longer cuts the top part of the Frame Layout Fix The navigation bar displays the apropriate icons

--- app/src/main/java/com/example/ensardz/pantallasenrique/MainActivity.java
+++ app/src/main/java/com/example/ensardz/pantallasenrique/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.ensardz.pantallasenrique;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,34 +10,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    NavigationView mNavigationView = null;
+    Toolbar mToolbar = null;
+    TextView prefectoTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        MainFragment mainFragment = new MainFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, mainFragment);
+        fragmentTransaction.commit();
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -81,18 +83,38 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        //TODO: Al reemplazar el fragment, parte del fragment layout se corta, posiblemente por la barra superior?
+        //TODO: Los siguientes botones solo reemplazan el frame_container, por lo que el titulo de la barra superior no se actualiza.
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_notificaciones) {
+            Toast.makeText(this, "Notificacion", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_reporte) {
+            getSupportActionBar().setTitle("Nuevo Reporte");
+            NuevoReporteFragment reporteFragment = new NuevoReporteFragment();
+            fragmentTransaction.replace(R.id.fragment_container, reporteFragment);
+            fragmentTransaction.commit();
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_recorrido) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_configuracion) {
 
-        } else if (id == R.id.nav_send) {
+            EscanerFragment escanerFragment = new EscanerFragment();
+            fragmentTransaction.replace(R.id.fragment_container, escanerFragment);
+            fragmentTransaction.commit();
 
+        } else if (id == R.id.nav_acerca_de) {
+
+        } else if (id == R.id.nav_ayuda) {
+
+        } else if (id == R.id.nav_cerrar_sesion){
+
+        } else if (id == R.id.nav_inicio){
+
+            getSupportActionBar().setTitle("NOMBRE");
+            MainFragment mainFragment = new MainFragment();
+            fragmentTransaction.replace(R.id.fragment_container, mainFragment);
+            fragmentTransaction.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -100,8 +122,28 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    public void iniciarRecorrido(View view){
-        Intent intent = new Intent(this, NuevoReporte.class);
-        startActivity(intent);
+    public void cancelarOnClick(View view){
+        Toast.makeText(this, "Has Cancelado", Toast.LENGTH_SHORT).show();
     }
+
+    public void aceptarOnClick(View view){
+        Toast.makeText(this, "Has Aceptado", Toast.LENGTH_SHORT).show();
+    }
+
+    public void escanerBarraOnClick(View view){
+        Toast.makeText(this, "Escaner barra", Toast.LENGTH_SHORT).show();
+    }
+
+    public void escanerQROnClick(View view){
+        Toast.makeText(this, "Escaner QR", Toast.LENGTH_SHORT).show();
+    }
+
+    public void recorridoOnClick(View view){
+        NuevoReporteFragment reporteFragment = new NuevoReporteFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, reporteFragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/ensardz/pantallasenrique/MainFragment.java
@@ -0,0 +1,44 @@
+package com.example.ensardz.pantallasenrique;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View fragmentMain = inflater.inflate(R.layout.fragment_main, container, false);
+        ImageButton fab = (ImageButton) fragmentMain.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        return fragmentMain;
+    }
+
+}
